0c7df5734c5f7f97e3d9cdbb416313b08fb369d2
Klali-Firas <firasklali1234@gmail.com>
2024-05-04 12:43:10 +0100

    authentification done

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -35,6 +35,7 @@ class RegistrationController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             // encode the plain password
+            $user->setPassword("ROLE_USER");
             $user->setPassword(
                 $userPasswordHasher->hashPassword(
                     $user,
